0820a28599f21c1346b0e7f61d583b3fcceaaa26
onemira <kangmira@yahoo.com>
2019-05-22 21:20:28 -0400

    fixed some errors

--- javascript/CountOddNumbersbelown.js
+++ javascript/CountOddNumbersbelown.js
@@ -1,3 +1,3 @@
-function oddCount(n){
-  return Math.floor(n/2);
+function oddCount(n) {
+  return Math.floor(n / 2)
 }
--- javascript/CountSheep.js
+++ javascript/CountSheep.js
@@ -1,7 +1,7 @@
-var countSheep = function (num){
-  let str = ‘’;
-  for (let i = 1; i < num + 1; i++){
+var countSheep = function(num) {
+  let str = ''
+  for (let i = 1; i < num + 1; i++) {
     str += `${i} sheep...`
   }
   return str
- }
+}
